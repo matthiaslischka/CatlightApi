85810d0c8d94555a5dd829475b98fba2a6c9814c
Matthias Lischka <mli@techtalk.at>
2017-01-13 21:05:53 +0100

    Batman Commit

---
+++ CatlightApi/Dashboard.cs
@@ -0,0 +1,16 @@
+﻿namespace CatlightApi
+{
+    public enum Severity
+    {
+        Ok,
+        Info,
+        Warning,
+        Critical
+    }
+
+    public class Dashboard
+    {
+        public Severity Severity { get; set; }
+        public bool IsAcknowledged { get; set; }
+    }
+}
---
+++ CatlightApiTests/UnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CatlightApi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CatlightApiTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void CallStatus_ThrowsNoException()
+        {
+            var catlight = new Catlight();
+            try
+            {
+                catlight.DoesNeedAttention();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+        }
+    }
+}
