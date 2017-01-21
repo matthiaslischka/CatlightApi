6eda02c73332e1b5792c31e84256d2d2a44e8cb9
Matthias Lischka <mli@techtalk.at>
2017-01-21 10:26:42 +0100

    added test for primitive performance measure

--- CatlightApiTests/UnitTests.cs
+++ CatlightApiTests/UnitTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Diagnostics;
 using CatlightApi;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,6 +13,39 @@ namespace CatlightApiTests
             var catlight = new Catlight();
             var doesNeedAttention = catlight.DoesNeedAttention();
         }
+
+        [Ignore]
+        [TestMethod]
+        public void CallStatus_StopwatchTime()
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            var catlight1 = new Catlight();
+            var isAcknowledged = catlight1.IsAcknowledged();
+            stopwatch.Stop();
+            var isAcknowledgedElapsedTime = stopwatch.Elapsed;
+
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            var catlight2 = new Catlight();
+            var severityLevel = catlight2.GetSeverityLevel();
+            stopwatch.Stop();
+            var severityLevelElapsedTime = stopwatch.Elapsed;
+
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            var catlight3 = new Catlight();
+            var doesNeedAttention = catlight3.DoesNeedAttention();
+            stopwatch.Stop();
+            var doesNeedAttentionElapsedTime = stopwatch.Elapsed;
+
+            Debug.WriteLine(
+                $"isAcknowledged = {isAcknowledged} in {isAcknowledgedElapsedTime}\n" +
+                $"severityLevel = {severityLevel} in {severityLevelElapsedTime}\n" +
+                $"doesNeedAttention = {doesNeedAttention} in {doesNeedAttentionElapsedTime}");
         }
     }
 }
