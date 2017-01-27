ee0187f02fce7b583649b866765e509e4b9840eb
Matthias Lischka <mli@techtalk.at>
2017-01-27 15:24:11 +0100

    Refactoring to one public method

---
+++ CatlightApi/CatlightConnector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace CatlightApi
+{
+    public class CatlightConnector
+    {
+        private readonly IPortHelper _portHelper;
+
+        public CatlightConnector()
+        {
+            _portHelper = new WindowsLogFileBasedPortHelper();
+        }
+
+        public CatlightStatus GetStatus()
+        {
+            var dashboard = UpdateDashboard();
+
+            if (dashboard == null)
+                return CatlightStatus.CatlightNotFound;
+
+            if (!dashboard.Servers.Any())
+                return CatlightStatus.NoProjects;
+
+            switch (dashboard.SeverityLevel)
+            {
+                case SeverityLevel.Ok:
+                    return CatlightStatus.Ok;
+
+                case SeverityLevel.Info:
+                    return CatlightStatus.Info;
+
+                case SeverityLevel.Warning:
+                    return dashboard.IsAcknowledged
+                        ? CatlightStatus.WarningAcknowledged
+                        : CatlightStatus.Warning;
+
+                case SeverityLevel.Critical:
+                    return dashboard.IsAcknowledged
+                        ? CatlightStatus.CriticalAcknowledged
+                        : CatlightStatus.Critical;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private Dashboard UpdateDashboard()
+        {
+            var portNumber = _portHelper.GetPortNumber();
+            if (!portNumber.HasValue)
+                return null;
+
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    var dashboardJsonString =
+                        webClient.DownloadString($"http://localhost:{portNumber}/Dashboard/GetDashboard");
+                    return JsonConvert.DeserializeObject<Dashboard>(dashboardJsonString);
+                }
+                catch (WebException)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ CatlightApi/Dto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace CatlightApi
+{
+    internal enum SeverityLevel
+    {
+        Ok,
+        Info,
+        Warning,
+        Critical
+    }
+
+    public enum CatlightStatus
+    {
+        CatlightNotFound,
+        NoProjects,
+        Ok,
+        Info,
+        WarningAcknowledged,
+        Warning,
+        CriticalAcknowledged,
+        Critical
+    }
+
+    internal class Dashboard
+    {
+        public SeverityLevel SeverityLevel { get; set; }
+        public bool IsAcknowledged { get; set; }
+        public List<Server> Servers { get; set; }
+    }
+
+    internal class Server
+    {
+        public string Name { get; set; }
+    }
+}
