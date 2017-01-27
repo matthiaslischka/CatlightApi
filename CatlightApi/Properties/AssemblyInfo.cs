e7098d4397249322b826f836113426a145261780
Matthias Lischka <mli@techtalk.at>
2017-01-27 18:42:48 +0100

    Minor Cleanup

--- CatlightApi/Properties/AssemblyInfo.cs
+++ CatlightApi/Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("CatlightApi")]
 [assembly: AssemblyDescription("Very basic API for the CatLight Status Notifications.")]
 [assembly: AssemblyConfiguration("")]
@@ -16,9 +17,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("89e0fad0-af09-4fbc-8358-83e9739a8ddf")]
 
 // Version information for an assembly consists of the following four values:
@@ -31,5 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
