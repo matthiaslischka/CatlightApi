f1554ba8e9b4d532acacd6188db88b6c53d1f213
Matthias Lischka <matthias.lischka@gmx.at>
2017-01-31 17:38:19 +0100

    Json Severity Level Deserialization Tests

--- CatlightApiTests/UnitTests.cs
+++ CatlightApiTests/UnitTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using CatlightApi;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace CatlightApiTests
 {
@@ -27,6 +29,38 @@ namespace CatlightApiTests
             var catlightStatusElapsedTime = stopwatch.Elapsed;
 
             Debug.WriteLine($"catlightStatus = {catlightStatus} in {catlightStatusElapsedTime}");
+        }
+
+        [TestMethod]
+        public void DeserializeDashboard_SeverityOk_SeverityIsOk()
+        {
+            var severityOkJson = "{\"severity\": \"Ok\"}";
+            var deserializeDashboard = JsonConvert.DeserializeObject<Dashboard>(severityOkJson);
+            deserializeDashboard.Severity.Should().Be(SeverityLevel.Ok);
+        }
+
+        [TestMethod]
+        public void DeserializeDashboard_SeverityInfo_SeverityIsInfo()
+        {
+            var severityInfoJson = "{\"severity\": \"Info\"}";
+            var deserializeDashboard = JsonConvert.DeserializeObject<Dashboard>(severityInfoJson);
+            deserializeDashboard.Severity.Should().Be(SeverityLevel.Info);
+        }
+
+        [TestMethod]
+        public void DeserializeDashboard_SeverityWarning_SeverityIsWarning()
+        {
+            var severityWarningJson = "{\"severity\": \"Warning\"}";
+            var deserializeDashboard = JsonConvert.DeserializeObject<Dashboard>(severityWarningJson);
+            deserializeDashboard.Severity.Should().Be(SeverityLevel.Warning);
+        }
+
+        [TestMethod]
+        public void DeserializeDashboard_SeverityCritical_SeverityIsCritical()
+        {
+            var severityCriticalJson = "{\"severity\": \"Critical\"}";
+            var deserializeDashboard = JsonConvert.DeserializeObject<Dashboard>(severityCriticalJson);
+            deserializeDashboard.Severity.Should().Be(SeverityLevel.Critical);
         }
     }
 }
